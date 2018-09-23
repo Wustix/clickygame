d207fbb93cfa89853e8cc5c9bbcffac39ecad3b6
unknown <wroehrman@yahoo.com>
2018-09-23 00:11:09 -0500

    update to files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          SPORTS CLIQ!!!
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div className="App-intro">
+          <p>
+            Click on every Sport once.  Whenever you click a Sport the grid will shuffle.
         </p>
+          <p>
+            If you click on a Sport more than once the game will restart!
+        </p>
+        </div>
       </div>
     );
   }
